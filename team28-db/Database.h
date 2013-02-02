7bbe495aacc28bae360db0aea0f899a352a831cc
mgyarmathy <mgyarmathy2011@gmail.com>
2013-02-02 17:03:45 -0600

    more function declarations added to Database.h

--- team28-db/Database.h
+++ team28-db/Database.h
@@ -1,22 +1,40 @@
 /*
 * Name        : Database.h
 * Author      : Team 28
-* Description : - header file for Department class
+* Description : - header file for Database class
 */
 
 #ifndef DATABASE_H_
 #define DATABASE_H_
+
+#include <string>
+#include <vector>
+#include "Table.h"
+
+using namespace std;
 
 class Database
 {
 	public:
 		Database() {};
 
-		add_table(Table t, string name);
-		drop_table(string name);
+		void add_table(Table t, string name);
+		void drop_table(string name);
+
+		void save(string filename);
+		void load(string filename);
+		void merge(Database d);
+		void copy(Database d);
+
+		vector<string> list_tables();
+		vector<Table> get_tables();
+
+		//query
+		//delete
+		//update
 
 	private:
 
-}
+};
 
 #endif //DATABASE_H_
