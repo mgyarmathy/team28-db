02bb255e9fe3e4bf3ac02132849d04ebac01ff99
Logan Elliott <badbob108@gmail.com>
2013-02-21 21:23:12 -0600

    more comments
    record.cpp comments

--- team28-db/Record.cpp
+++ team28-db/Record.cpp
@@ -9,7 +9,9 @@ Record::Record(vector<string> data) {
 string& Record::elementAt(int index) {
 	if(index >= entries.size())
 		throw OutOfBoundsException();
+	//initially check if the index is larger than the record size
 	else if(index < 0)
+	//then check that the index is a vlid number greater than 0
 		throw OutOfBoundsException(); 
 	else
 	return entries[index];
